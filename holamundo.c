52180394b6f370bd96d1643bb213b03028110b2a
darkyuyo <ajllf7@gmail.com>
2020-09-15 16:47:42 -0300

    fena meada

--- holamundo.c
+++ holamundo.c
@@ -1,25 +1,21 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
 int main()
-{
-    const int layer1 = 5;
-    const int layer2 = 10;
-    const int outfiles = 100;
-
-    char buffer[100];
-    for (int i = 0; i < layer1; ++i)
-    {
-        sprintf(buffer, "mkdir folder%d", i);
-        system(buffer);
+{   
+    int i=1;
+    DIR *dir;
+    struct dirent *dp;
+    const char *path="./Juegos"; 
+    FILE *file;
+    dir = opendir(path);
+    while(dp=readdir(dir)){
+        dp->d_name;
+        print("%d",i);
+        i++;
     }
-    int i = 0;
-    sprintf(buffer, "mv README folder%d",i);
-    system(buffer);
-    printf("holaaaaa");
-    sprintf(buffer, "cd /home/ubuntu/Desktop/SO/tareaSO/folder0 && mv README ..");
-    system(buffer);
-    return EXIT_SUCCESS;
+    return 0;
 }
