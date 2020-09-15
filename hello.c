38bc6f83a9d4c671f5da9982b120b9f2ce6dbf96
Sofia <sofia.manana@sansano.usm.cl>
2020-09-15 20:48:16 +0000

    Leer dir listo

--- hello.c
+++ hello.c
@@ -6,18 +6,26 @@
 #include <sys/stat.h>
 int main()
 {   
-    int i=1;
+    int i=0;
     DIR *dir;
     struct dirent *dp;
     const char *path="./Juegos"; 
+    char direc[300];
     FILE *file;
     dir = opendir(path);
     if(dir==NULL){
         printf("No se abrio el directorio");
         exit(2);
     }
-    while((dp=readdir(dir))!=NULL){
-        printf("%d\t%d\t%d\t%s\n",dp->d_ino,dp->d_off,dp->d_reclen,dp->d_name);
+    while( (dp=readdir(dir)) ){
+        if ((strcmp(dp->d_name,".") == 0) || (strcmp(dp->d_name,"..") == 0)){		//elimina carpeta "." y ".."
+	    }
+        else{
+            i++;
+            printf("File %d: %s\n", i, dp->d_name);         
+
+
+        }
     }
     closedir(dir);
     return 0;
