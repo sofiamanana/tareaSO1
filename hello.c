9c4fc798f67dde039113f73a546dd3eb15bfeb62
darkyuyo <ajllf7@gmail.com>
2020-09-20 18:39:27 -0300

    poto

--- hello.c
+++ hello.c
@@ -8,13 +8,18 @@
 void ordenar(){
     int i=0;
     DIR *dir;
+    DIR *dir2;
     struct dirent *dp;
     const char *path="./Juegos"; 
     char direc[300];
     char titulo[300];
     char genero[300];
+    char compania[300];
+    char resumen[300];
     char *aux;
+    char aux2[300];
     FILE *fp;
+    FILE *fp2;
     dir = opendir(path);
     if(dir==NULL){
         printf("No se abrio el directorio");
@@ -34,21 +39,34 @@ void ordenar(){
             fp = fopen(direc, "r");
             fgets(titulo, sizeof(char)*300, fp);
             fgets(genero, sizeof(char)*300, fp);
+            fgets(compania, sizeof(char)*300, fp);
+            fgets(resumen, sizeof(char)*300, fp);
             printf("Genero: %s", genero);
-
             aux = strtok(genero, ",");
-            while(aux != NULL){
-                printf("Genero: %s\n", aux);
-                aux = strtok(NULL, ",");
+            printf("%s\n",aux);
+            strcpy(aux2, "./");
+			strcat(aux2, aux);
+            printf("%s\n", aux2);
+            dir2 = opendir(aux2);
+            if(dir2==NULL){
+                printf("no existe esa carpeta\n");
+                mkdir(aux,0700);
             }
-            
+            else{
+                printf("si existe esa carpeta\n");
+            }
+            fp2=fopen(dp->d_name, "a"); 
+            fclose (fp2 );
+            fclose(fp);
+
         }
-    }
+    }   
     closedir(dir);
 }
 
 int main(){
     printf("Bienvenido!\n Presione 1 si desea ordenar la carpeta Juegos.\n Presione 2 si desea recorrer los archivos.\n");
+    ordenar();
     
 
     return 0;
