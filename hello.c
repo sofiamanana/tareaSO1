dee34de22d93a21ca6199d637f8c95562fd699f2
Sofia <sofia.manana@sansano.usm.cl>
2020-09-23 16:46:01 +0000

    Navegar ready

--- hello.c
+++ hello.c
@@ -70,24 +70,31 @@ void ordenar(){
     closedir(dir);
 }
 
+
 void navegar(){
     char actual[300];
-    char nombre_carp[300];
-    int opcion;
-    char opcion2[300];
+    char nombre[300];
+    char opcion[300];
+    char titulo[300];
+    char genero[300];
+    char company[300];
+    char descr[300];
+    char *aux;
     DIR *dir;
+    FILE *fp;
     struct dirent *dp;
     
-    while(opcion != 3){
+    while(strcmp(opcion,"4\n")!=0){
         getcwd(actual,sizeof(actual)); 
         dir = opendir(actual);
-        printf("La ubicacion actual es:%s y contiene:\n\n",actual);
+        printf("\nLa ubicacion actual es:%s y contiene:\n\n",actual);
         if(dir==NULL){
             printf("No se abrio el directorio");
             exit(2);
         }
+        int i=0;
         while( (dp=readdir(dir)) ){
-            int i=0;
+            
             if ((strcmp(dp->d_name,".") == 0) || (strcmp(dp->d_name,"..") == 0)){		
             }
             else{
@@ -100,15 +107,35 @@ void navegar(){
         printf("2. Para volver atras.\n");
         printf("3. Abrir un txt.\n");
         printf("4. Salir.\n");
-        fgets(opcion2,100,stdin);
+        fgets(opcion,100,stdin);
 
-        if(strcmp(opcion2,"1\n")==0){
+        if(strcmp(opcion,"1\n")==0){
             printf("Escriba nombre de la carpeta:\n");
-            fgets(nombre_carp,100,stdin);
+            fgets(nombre,100,stdin);
+            aux = strtok(nombre,"\n");
+            strcat(actual,"/");
+            strcat(actual,aux);
+            chdir(actual);
+        }
+        else if(strcmp(opcion,"2\n")==0){
+            strcat(actual,"/");
+            strcat(actual,"..");
+            chdir(actual);
+        }
+        else if(strcmp(opcion,"3\n")==0){
+            printf("Escriba nombre de la archivo (archivo.txt):\n");
+            fgets(nombre,100,stdin);
+            aux = strtok(nombre,"\n");
+            fp = fopen(aux,"r");
+            fgets(titulo, sizeof(char)*300, fp);
+            fgets(genero, sizeof(char)*300, fp);
+            fgets(company, sizeof(char)*300, fp);
+            fgets(descr, sizeof(char)*300, fp);
 
-            strcpy(actual,"/");
-            strcpy(actual,nombre_carp);
-
+            printf("\nTitulo: %s\n",titulo);
+            printf("Genero: %s\n",genero);
+            printf("Company: %s\n",company);
+            printf("Descripcion: %s\n",descr);
         }
     }
 }
